529481380a632b11d0b93a07476b8f01f8b50a42
newtoncutrim <newtoncutrim777@gmail.com>
2022-11-15 09:52:55 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Thanks for downloading this template!
+## titulo 2
+
+```
+Template Name: OnePage
+Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
+```
---
+++ assets/js/modal.js
@@ -0,0 +1,36 @@
+// alert('PORTIFOLIO EM EM FASE DE FINALIZAÇAO')
+
+setTimeout(apresentarModal, 3000)
+function apresentarModal() {
+
+    var modal = document.querySelector(".modal")
+
+    if (modal != null) {
+
+        document.querySelector('.modal').style.display = 'block'
+
+        document.querySelector('.modal a').addEventListener('click', function () {
+            document.querySelector('.modal').style.display = 'none'
+        })
+
+
+    }
+}
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 5000)
+
+
+function nextImage(){
+    count++
+    if(count > 5) {
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
+
